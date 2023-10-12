16afd546df10a7c25c873f20a6e2764e18b9581e
Nibras Sameeh <nibras.s@aeturnum.com>
2023-10-12 16:51:48 +0530

    Added chakra-ui, CORS setings

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,10 +1,57 @@
-import SidebarWithHeader from "./shared/SideBar.jsx";
-import {Button} from "@chakra-ui/react";
+import SidebarWithHeader from "./components/shared/SideBar.jsx";
+import {Spinner, Text, Wrap, WrapItem} from "@chakra-ui/react";
+import {useEffect, useState} from "react";
+import CustomerService from "./services/CustomerService.js";
+import CardWithImage from "./components/Card.jsx";
 
 function App() {
+
+    const [customers, setCustomers] = useState([]);
+    const [loadingCustomers, setLoadingCustomers] = useState(false);
+
+    useEffect(() => {
+        setLoadingCustomers(true);
+        CustomerService.getCustomers()
+            .then(res => {
+                setCustomers(res.data)
+            })
+            .catch(err => {
+                console.error(err);
+            })
+            .finally(() => {
+                setLoadingCustomers(false);
+            })
+    }, []);
+
+    if (loadingCustomers) {
+        return (
+            <SidebarWithHeader>
+                <Spinner thickness='4px'
+                         speed='0.65s'
+                         emptyColor='gray.200'
+                         color='blue.500'
+                         size='xl'/>
+            </SidebarWithHeader>
+        )
+    }
+
+    if (customers && customers.length === 0) {
+        return (
+            <SidebarWithHeader>
+                <Text>No customers available</Text>
+            </SidebarWithHeader>
+        )
+    }
+
     return (
         <SidebarWithHeader>
-            <Button colorScheme='teal' size='lg'>Click Me</Button>
+            <Wrap justify={"center"} spacing={"30px"}>
+                {customers.map((customer, index) => (
+                    <WrapItem key={index}>
+                        <CardWithImage {...customer}/>
+                    </WrapItem>
+                ))}
+            </Wrap>
         </SidebarWithHeader>
     )
 }
